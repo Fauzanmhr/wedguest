f4437fbf8da1b3f53c2e1e96f4cbccd8db8e1529
Fauzanmhr <fauzan432003@gmail.com>
2024-06-12 21:22:26 +0700

    add retry function to database config

--- config/database.js
+++ config/database.js
@@ -5,6 +5,31 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   host: process.env.DB_HOST,
   dialect: 'mariadb',
   logging: false,
+  retry: {
+    max: 5, // Number of retry attempts
+    match: [
+      /ECONNREFUSED/,
+      /ETIMEDOUT/,
+      /EHOSTUNREACH/,
+      /EPIPE/,
+      /ECONNRESET/
+    ],
+    backoffBase: 1000, // Initial backoff duration in ms. Default is 100ms.
+    backoffExponent: 1.5 // Exponent to increase backoff duration each attempt. Default is 2.
+  }
 });
+
+async function connectWithRetry() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database has been established successfully.');
+  } catch (err) {
+    console.error('Unable to connect to the database:', err);
+    console.log('Retrying in 5 seconds...');
+    setTimeout(connectWithRetry, 5000); // Retry after 5 seconds
+  }
+}
+
+connectWithRetry();
 
 module.exports = sequelize;
